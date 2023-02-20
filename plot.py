4d1305add988c3ceae862b4e609e2ded935aa31b
felipecerda <felipecerdasaavedra@gmail.com>
2023-02-20 18:48:57 -0300

    codigo para graficar rewards a partir de csv

---
+++ plot.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read in the csv file
+df = pd.read_csv('rewards.csv')
+df = df[df.index < 500]
+# Use the index to plot the rewards
+plt.plot(df.index, df['reward'])
+
+# Show the plot 
+plt.show()
