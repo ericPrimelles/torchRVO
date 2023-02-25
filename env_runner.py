cf7a0c2040cd38e0c8c12133dde4d50d926f9431
Felipe Cerda <felipecerdasaavedra@gmail.com>
2023-02-24 22:35:18 -0300

    runner que corre varias instancias con parametros especificados en archivo csv

--- env_runner.py
+++ env_runner.py
@@ -1,30 +1,54 @@
 import multiprocessing as mp
-from multiprocessing.pool import ThreadPool as tp
+from multiprocessing.pool import ThreadPool
 import subprocess as sp
 import shlex
-import time
+import pandas as pd
+import sys
 
-def run_program(cmd):    
-    #sp.call(shlex.split(cmd))  # This will block until cmd finishes
+class Run:
+    def __init__(self, pid: int, epochs: int, episodes: int,
+                  frame_count: int, buffer_len: int, batch_size: int, train: int) -> None:
+        self.pid = pid
+        self.epochs = epochs
+        self.episodes = episodes
+        self.frame_count = frame_count
+        self.buffer_len = buffer_len
+        self.batch_size = batch_size
+        self.train = train
+
+    def get_args(self):
+        return f'{self.frame_count} {self.buffer_len} {self.batch_size} {self.epochs} {self.episodes} {self.train} {self.pid}'
+
+
+def parse_csv(filename: str)-> list[Run]:
+    runs: list[Run] = []
+    df: pd.DataFrame = pd.read_csv(filename)
+    for _, row in df.iterrows():
+        runs.append(Run(row['pid'], row['epochs'], row['episodes'], 
+            row['frame_count'], row['buffer_len'], row['batch_size'], row['train']))
+    return runs 
+
+def call_proc(cmd):    
     p = sp.Popen(shlex.split(cmd), stdout=sp.PIPE, stderr=sp.PIPE)
     out, err = p.communicate()
     return (out, err)
 
-
-if __name__ == "main":
+if __name__ == "__main__":
+    # parámetros: filename
+    filename = sys.argv[1]
+    # runs
+    runs = parse_csv(filename)
+    results = []
+    pool = ThreadPool(mp.cpu_count())
     process_list = []
-
     # Create a list of processes
-    for i in range(10):
-        p = mp.Process(target=run_program, args=("My Program", {"param1": "val1", "param2": i}))
-        process_list.append(p)
-
-    # Limit the number of processes running at a time
-    max_processes = 2 
-    while process_list:
-        while len(mp.active_children()) < max_processes and process_list:
-            process = process_list.pop()
-            process.start()
-        
-        time.sleep(0.2)
-
+    for run in runs:
+        cmd = f'python3 MADDPG.py {run.get_args()}'
+        results.append(pool.apply_async(call_proc, args=(cmd,)))
+    
+    pool.close()
+    pool.join()
+    for result in results:
+        out, err = result.get()
+        print("out: {} err: {}".format(out, err))
+    
